da6c98888463557a84746b16cd1cf3e82ce3b6c3
sampdubs <sammypw1@gmail.com>
2021-08-01 09:31:32 -0400

    Added problem 6

---
+++ 63.py
@@ -0,0 +1,20 @@
+from tqdm import trange
+
+def powFast(base, power):
+    result = 1
+    for _ in range(power):
+        result *= base
+    return result
+
+# arbitrarily choose 100 as the upper bound
+total = 0
+for power in range(1, 100):
+    lower = powFast(10, power - 1)
+    upper = lower * 10
+    lower -= 1
+
+    for base in range(1, 10):
+        if lower < powFast(base, power) < upper:
+            total += 1
+
+print(total)
